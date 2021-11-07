41b262e8831a3c3302044a467aabf41e8c2289ba
Canyon Smith <canyonfsmith@gmail.com>
2021-11-07 01:09:00 -0600

    asdf

---
+++ account_types.py
@@ -0,0 +1,16 @@
+cash_accounts = [
+    'Food',
+    'Rainy Day Fund',
+    'Travel Funds',
+    'MYSTYLE CHECKING',
+    'Anytime Checking',
+    'Visa Account',
+    'Young Savers',
+    'SAVINGS',
+    'REWARDS VISA PLATINUM']
+
+investment_accounts = [
+    'Individual Brokerage',
+    'ROTH IRA',
+    'Robinhood Investments'                        
+]
